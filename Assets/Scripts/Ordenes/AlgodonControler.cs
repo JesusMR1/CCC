f308d59049aa1cfa9b38925067fee6ff90af907d
JesusMR1 <A01703076@tec.mx>
2023-11-27 22:52:00 -0600

    dasd

--- Assets/Scripts/Ordenes/AlgodonControler.cs
+++ Assets/Scripts/Ordenes/AlgodonControler.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[System.Serializable]
+public struct EstructuraAlgodon
+{
+    public Texture2D colorAlgodon;
+}
 public class AlgodonControler : MonoBehaviour
 {
+    public ScriptAlgodon algodonPrefab;
+
+    public EstructuraAlgodon[] structArray = new EstructuraAlgodon[3];
+     
     // Start is called before the first frame update
     void Start()
     {
-        
+        ScriptAlgodon newAlgodon = Instantiate(algodonPrefab);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        ScriptAlgodon newAlgodon = algodonPrefab;
+        if (Input.GetMouseButtonDown(0) && tag == "ColorAzul")
+        {
+            newAlgodon.texturaAlgodon = structArray[0].colorAlgodon;
+        }
     }
 }
--- Assets/Scripts/Ordenes/ScriptAlgodon.cs
+++ Assets/Scripts/Ordenes/ScriptAlgodon.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ScriptAlgodon : MonoBehaviour
 {
 
     public Material algodonMat;
@@ -12,9 +12,11 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
-    public void CambiarTextura()
+    public void CambiarTextura(Texture2D nuevaTextura)
     {
-        algodonMat.SetTexture("_Base_Color_Texture", texturaAlgodon);
+        algodonMat = GetComponent<Material>();
+        algodonMat.SetTexture("_Base_Color_Texture", nuevaTextura);
+        texturaAlgodon = nuevaTextura;
     }
 
 }
