9a3f0178d14578a0d073e2b3eabeba924f17fb98
yckjm <a01709559@tec.mx>
2023-11-30 13:28:31 -0600

    asd
    sad

--- Assets/Limpiar/ButtonManager.cs
+++ Assets/Limpiar/ButtonManager.cs
@@ -1,12 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class ButtonManager : MonoBehaviour
 {
-
-
     public Button[] buttons;          // Array to hold all buttons
     public GameObject firstObject;    // The first 3D object
     public GameObject secondObject;   // The second 3D object
@@ -84,9 +81,6 @@ public class ButtonManager : MonoBehaviour
         // Reset the state when a new scene is loaded
         ResetState();
     }
-
-
-
 
     void ResetState()
     {
--- Assets/SceneTransition.cs
+++ Assets/SceneTransition.cs
@@ -3,43 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-using UnityEditorInternal;
 
 public class SceneTransition : MonoBehaviour
 {
-
-    [SerializeField] Animator transitionAnim;
-
     // Specify the name of the scene you want to load
     public string sceneToLoad;
 
     // Function to be called when the button is clicked
-
     public void LoadScene()
     {
-        StartCoroutine(WaitTimer());
-    }
-
-    public void NextLevel()
-    {
-        StartCoroutine(AnimScene());
-    }
-
-    IEnumerator AnimScene()
-    {
-        transitionAnim.SetTrigger("End");
-        yield return new WaitForSeconds(1);
-    }
-
-    IEnumerator WaitTimer()
-    {
-        yield return new WaitForSeconds(1);
+        // Load the specified scene
         SceneManager.LoadScene(sceneToLoad);
-
-
     }
-
-
-
-
 }
