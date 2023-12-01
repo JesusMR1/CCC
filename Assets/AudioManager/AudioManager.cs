ccbaad73907d03086f5565dac471bde5af65fa2a
yckjm <a01709559@tec.mx>
2023-12-01 15:30:03 -0600

    sdfghjkl

---
+++ Assets/AudioManager/AudioManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // audio source
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    //audio clip
+
+    public AudioClip background;
+    public AudioClip death;
+    public AudioClip checkpoint;
+    //more public sound clips
+
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+
+
+}
